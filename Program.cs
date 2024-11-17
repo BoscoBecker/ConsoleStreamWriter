04f0147a86570974fc0a18a37c2e69ab69fa85d6
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-11-17 10:35:17 -0300

    Add structure

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ConsoleStreamWriter {
+    internal abstract class Program {
+       private static void Main()
+       {
+           const string sourcePath = @"C:\temp\File.txt";
+           const string targetPath = @"C:\temp\File1.txt";
+
+           try
+           {
+               var lines = File.ReadAllLines(sourcePath);
+               var sw = File.AppendText(targetPath);
+               try
+               {
+                   foreach (var line  in lines)
+                   {
+                       sw.WriteLine(line.ToUpper());
+                   }
+               }
+               finally
+               {
+                   sw.Close();
+                   sw.Dispose();
+               }
+           }
+           catch (Exception e)
+           {
+               Console.WriteLine(e);
+               throw;
+           }
+
+       }
+    }
+};
+
+
